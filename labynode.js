39cb49862ce80a0dc2afd796f59d03a8f067a2b5
Johannes M. Dietze <58228327+6lyxt@users.noreply.github.com>
2020-06-22 22:54:23 +0200

    oopsie

--- labynode.js
+++ labynode.js
@@ -4,17 +4,17 @@ const getEmote = require("./utils/getEmotes")
 
 module.exports = {
     getStatus: async function getStatus(callback) {
-        needle.get("https://dl.labymod.net/", (function (error, response) {
+        await needle.get("https://dl.labymod.net/", (function (error, response) {
             if (error) return error;
             callback(JSON.parse(response.body).status);
         }))
     },
     getCosmetics: async function getCosmetics(uuid, callback){
-        getCosmetic.getCosmetics(uuid, callback)
+        await getCosmetic.getCosmetics(uuid, callback)
     },
 
     getEmotes: async function getEmotes(uuid, callback){
-        getEmote.getEmotes(uuid, callback);
+        await getEmote.getEmotes(uuid, callback);
     }
     
     
